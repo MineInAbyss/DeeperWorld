56382626bf4320418e47ea1c84681cdf96dcd662
David Friedman <dgfried@gmail.com>
2020-04-05 11:44:46 -0700

    Permissions for LINFO (#24)
    * Add permission node for linfo

--- src/main/java/com/derongan/minecraft/deeperworld/Permissions.java
+++ src/main/java/com/derongan/minecraft/deeperworld/Permissions.java
@@ -2,5 +2,4 @@ package com.derongan.minecraft.deeperworld;
 
 public class Permissions {
     public static final String CHANGE_SECTION_PERMISSION = "deeperworld.changesection";
-    public static final String SET_CHANGE_SECTION_PERMISSION = "deeperworld.setchangesection";
 }
