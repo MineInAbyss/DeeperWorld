ef5f3547a8bb160ce245d9fa9a2bc29524b90603
Norazan <thendjes17@gmail.com>
2021-01-04 23:06:37 +0100

    Update command structure to a more logical setup.

--- src/main/java/com/derongan/minecraft/deeperworld/DeeperCommandExecutor.kt
+++ src/main/java/com/derongan/minecraft/deeperworld/DeeperCommandExecutor.kt
@@ -1,5 +1,6 @@
 package com.derongan.minecraft.deeperworld
 
+import com.derongan.minecraft.deeperworld.MinecraftConstants.FULL_DAY_TIME
 import com.derongan.minecraft.deeperworld.config.DeeperConfig
 import com.derongan.minecraft.deeperworld.services.WorldManager
 import com.derongan.minecraft.deeperworld.services.canMoveSections
@@ -8,6 +9,7 @@ import com.mineinabyss.idofront.commands.arguments.booleanArg
 import com.mineinabyss.idofront.commands.arguments.intArg
 import com.mineinabyss.idofront.commands.execution.ExperimentalCommandDSL
 import com.mineinabyss.idofront.commands.execution.IdofrontCommandExecutor
+import com.mineinabyss.idofront.commands.execution.stopCommand
 import com.mineinabyss.idofront.commands.extensions.actions.playerAction
 import com.mineinabyss.idofront.messaging.info
 import com.mineinabyss.idofront.messaging.success
@@ -16,20 +18,19 @@ import com.mineinabyss.idofront.messaging.success
 object DeeperCommandExecutor : IdofrontCommandExecutor() {
     override val commands: CommandHolder = commands(deeperWorld) {
         ("deeperworld" / "dw") {
-            "tp" {
+            "tp"(desc = "Enables or disables automatic teleports between sections for a player") {
                 val state by booleanArg()
-                playerAction{
-                    if(state){
+                playerAction {
+                    if (state) {
                         player.canMoveSections = true
                         sender.success("Automatic TP enabled for ${player.name}")
-                    }
-                    else{
+                    } else {
                         player.canMoveSections = false
                         sender.success("Automatic TP disabled for ${player.name}")
                     }
                 }
             }
-            "linfo" {
+            "linfo"(desc = "Gets information about a players section") {
                 playerAction {
                     val section = WorldManager.getSectionFor(player.location)
                     if (section == null)
@@ -38,28 +39,32 @@ object DeeperCommandExecutor : IdofrontCommandExecutor() {
                         sender.info("${player.name} is in section ${section.key}")
                 }
             }
-            "time" {
+            "time"(desc = "<set/add>") {
                 val time by intArg()
-                "set"{
-                    action {
+                "set"(desc = "Set the time of the main synchronization world and the other worlds with their respective offsets") {
+                    playerAction {
                         DeeperConfig.data.time.mainWorld?.let { world ->
                             world.time = time.toLong()
-                        } ?: return@action
+                        } ?: command.stopCommand("No main world specified for time synchronization. Check the config!")
 
                         DeeperConfig.data.time.syncedWorlds.forEach { (world, offset) ->
-                            world.time = time.toLong() + offset
+                            world.time = (time.toLong() + offset) % FULL_DAY_TIME
                         }
+
+                        sender.success("Set synced time to $time")
                     }
                 }
-                "add"{
-                    action {
+                "add"(desc = "Add to the main synchronization world time and the other worlds with their respective offsets") {
+                    playerAction {
                         DeeperConfig.data.time.mainWorld?.let { mainWorld ->
                             mainWorld.time += time.toLong()
 
                             DeeperConfig.data.time.syncedWorlds.forEach { (world, offset) ->
-                                world.time = (mainWorld.time + offset) + time.toLong()
+                                world.time = (mainWorld.time + offset) % FULL_DAY_TIME
                             }
-                        }
+
+                            sender.success("Added $time to synced time")
+                        } ?: command.stopCommand("No main world specified for time synchronization. Check the config!")
                     }
                 }
             }
--- src/main/java/com/derongan/minecraft/deeperworld/MinecraftConstants.kt
+++ src/main/java/com/derongan/minecraft/deeperworld/MinecraftConstants.kt
@@ -2,4 +2,5 @@ package com.derongan.minecraft.deeperworld
 
 object MinecraftConstants {
     const val WORLD_HEIGHT = 256
+    const val FULL_DAY_TIME = 24000
 }
