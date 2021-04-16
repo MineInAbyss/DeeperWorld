59ff744ce255ff847ea6b76a2357bf7b10f5de65
Boy <sivertpaulsen2@gmail.com>
2021-04-16 10:14:17 +0200

    Added a check to if they can go between sections for OOB tp

--- src/main/java/com/derongan/minecraft/deeperworld/listeners/PlayerListener.kt
+++ src/main/java/com/derongan/minecraft/deeperworld/listeners/PlayerListener.kt
@@ -1,5 +1,6 @@
 package com.derongan.minecraft.deeperworld.listeners
 
+import com.derongan.minecraft.deeperworld.services.canMoveSections
 import com.derongan.minecraft.deeperworld.world.section.section
 import com.mineinabyss.idofront.destructure.component1
 import com.mineinabyss.idofront.destructure.component2
@@ -16,9 +17,9 @@ object PlayerListener : Listener {
     @EventHandler
     fun onPlayerTeleport(event: PlayerTeleportEvent) {
         val (player, _, to, cause) = event
-        if (player.gameMode == SURVIVAL
-            && (cause == ENDER_PEARL || cause == CHORUS_FRUIT)
-            && to?.section == null
+        if ((player.gameMode == SURVIVAL
+                && (cause == ENDER_PEARL || cause == CHORUS_FRUIT)
+                && to?.section == null) && player.canMoveSections
         ) {
             event.isCancelled = true
         }
