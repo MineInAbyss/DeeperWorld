b50c9185c2f32dbac0cd8e9ee5efac97fda463f1
Scyu_ <scyu@scyu.dev>
2021-08-17 18:03:51 +0100

    Cleanup code

--- src/main/java/com/derongan/minecraft/deeperworld/listeners/PlayerListener.kt
+++ src/main/java/com/derongan/minecraft/deeperworld/listeners/PlayerListener.kt
@@ -4,7 +4,6 @@ import com.derongan.minecraft.deeperworld.services.canMoveSections
 import com.derongan.minecraft.deeperworld.world.section.inSectionTransition
 import com.derongan.minecraft.deeperworld.world.section.section
 import com.mineinabyss.idofront.messaging.error
-import com.mineinabyss.idofront.messaging.info
 import org.bukkit.GameMode.CREATIVE
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -15,9 +14,9 @@ import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL
 object PlayerListener : Listener {
     @EventHandler
     fun PlayerTeleportEvent.onPlayerTeleport() {
-        if (player.gameMode == CREATIVE) return player.info("Creative mode allowed")
-        if (cause != ENDER_PEARL && cause != CHORUS_FRUIT) return player.info("non epearl tp allowed")
-        if (!player.canMoveSections) return player.info("dw tp off")
+        if (player.gameMode == CREATIVE) return
+        if (cause != ENDER_PEARL && cause != CHORUS_FRUIT) return
+        if (!player.canMoveSections) return
 
         if (
             to.section == null ||
