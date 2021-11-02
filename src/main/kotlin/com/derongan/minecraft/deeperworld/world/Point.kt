bf51a8d884d5c16d30f72987c06a84105b9928de
Boy <sivertpaulsen2@gmail.com>
2021-11-02 12:26:52 +0100

    remove length func

--- src/main/kotlin/com/derongan/minecraft/deeperworld/world/Point.kt
+++ src/main/kotlin/com/derongan/minecraft/deeperworld/world/Point.kt
@@ -1,7 +1,5 @@
 package com.derongan.minecraft.deeperworld.world
 
-import kotlin.math.sqrt
-
 /**
  * Represents a single X/Y/Z cube in a minecraft world.
  */
@@ -13,6 +11,4 @@ data class CubePoint(val x: Int, val y: Int, val z: Int) {
     operator fun div(o: Float) = CubePoint((x / o).toInt(), (y / o).toInt(), (z / o).toInt())
 
     operator fun div(o: Int) = CubePoint(x / o, y / o, z / o)
-
-    val length get() = sqrt((x * x + z * z).toDouble())
 }
