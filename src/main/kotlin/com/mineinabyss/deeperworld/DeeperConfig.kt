10a21b71e7b79b7239d1132943f6fe83424f1c41
Boy <sivertpaulsen2@gmail.com>
2022-11-07 11:42:40 +0100

    change config classes

--- src/main/kotlin/com/mineinabyss/deeperworld/DeeperConfig.kt
+++ src/main/kotlin/com/mineinabyss/deeperworld/DeeperConfig.kt
@@ -12,34 +12,31 @@ import org.bukkit.World
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
-val deeperConfig get() = deeperWorld.config.data.deeperConfig
+val deeperConfig get() = deeperWorld.config.data
 @Serializable
-data class DeeperWorldConfig(val deeperConfig: DeeperConfig = DeeperConfig()) {
-    @Serializable
-    data class DeeperConfig(
-        val sections: List<Section> = emptyList(),
-        val damageOutsideSections: Double = 0.0,
-        val damageExcludedWorlds: Set<@Serializable(with = WorldSerializer::class) World> = emptySet(),
-        val remountPacketDelay: Duration = 40.ticks,
-        val fall: FallDamageConfig = FallDamageConfig(),
-        val time: TimeConfig = TimeConfig(),
-    ) {
-        val worlds = sections.map { it.world }.toSet()
-    }
+data class DeeperWorldConfig(
+    val sections: List<Section> = emptyList(),
+    val damageOutsideSections: Double = 0.0,
+    val damageExcludedWorlds: Set<@Serializable(with = WorldSerializer::class) World> = emptySet(),
+    val remountPacketDelay: Duration = 40.ticks,
+    val fall: FallDamageConfig = FallDamageConfig(),
+    val time: TimeConfig = TimeConfig(),
+) {
+    val worlds = sections.map { it.world }.toSet()
 
     @Serializable
     data class FallDamageConfig(
         val maxSafeDist: Float = -1f,
         val fallDistanceDamageScaler: Double = 0.01,
         val startingDamage: Double = 1.0,
-        val hitDelay: Duration = 10.ticks,
+        val hitDelay: @Serializable(DurationSerializer::class) Duration = 10.ticks,
         val spawnParticles: Boolean = true
     )
 
     @Serializable
     data class TimeConfig(
-        val updateInterval: Duration = 1800.seconds,
-        val mainWorld: World? = null,
-        val syncedWorlds: Map<World, Long> = emptyMap(),
+        val updateInterval: @Serializable(DurationSerializer::class) Duration = 1800.seconds,
+        val mainWorld: @Serializable(WorldSerializer::class) World? = null,
+        val syncedWorlds: Map<@Serializable(WorldSerializer::class) World, Long> = emptyMap(),
     )
 }
