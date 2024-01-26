93032c773152ae786a1fd4f5756c35cf92b4a568
Boy <sivertpaulsen2@gmail.com>
2024-01-26 15:41:10 +0100

    fix: dismounting vehicle when above-block is outside of sections

--- src/main/kotlin/com/mineinabyss/deeperworld/listeners/PlayerListener.kt
+++ src/main/kotlin/com/mineinabyss/deeperworld/listeners/PlayerListener.kt
@@ -1,15 +1,21 @@
 package com.mineinabyss.deeperworld.listeners
 
 import com.mineinabyss.deeperworld.services.canMoveSections
+import com.mineinabyss.deeperworld.world.section.correspondingSection
+import com.mineinabyss.deeperworld.world.section.inSectionOverlap
 import com.mineinabyss.deeperworld.world.section.inSectionTransition
 import com.mineinabyss.deeperworld.world.section.section
+import com.mineinabyss.idofront.location.up
+import com.mineinabyss.idofront.messaging.broadcastVal
 import com.mineinabyss.idofront.messaging.error
 import org.bukkit.GameMode.CREATIVE
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDismountEvent
 import org.bukkit.event.player.PlayerTeleportEvent
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.CHORUS_FRUIT
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.ENDER_PEARL
+import org.bukkit.event.vehicle.VehicleCreateEvent
 
 object PlayerListener : Listener {
     @EventHandler
@@ -25,6 +31,13 @@ object PlayerListener : Listener {
             player.error("Teleportation is disabled between Layers and Sections.")
             isCancelled = true
         }
+    }
+
+    @EventHandler
+    fun EntityDismountEvent.onCreateVehicle() {
+        if (entity.location.up(1).section != null) return
+        entity.error("The Abyss prevents you from dismounting here...")
+        isCancelled = true
     }
 }
 
